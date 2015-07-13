2e3619dcdf507d92bf68dbf422562a01cfbfe735
Thomas Ongeri <ongerit@gmail.com>
2015-07-13 14:16:18 -0400

    added slider, slider images, final push

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
 	
 // http://baijs.com/tinycarousel/	
 
+$("#slider7").tinycarousel({ interval : true});
 
-	
+
 // Built with	
 	console.log('This site was created using sass, susy, compass, breakpoints, bower, grunt, jquery, mmenu and npm:: by Thomas Ongeri');
 	
