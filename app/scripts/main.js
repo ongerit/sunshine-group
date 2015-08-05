9c1771107252ebaae0a443a53bc9ff4a9180b86c
Thomas Ongeri <ongerit@gmail.com>
2015-08-04 21:28:48 -0400

    clean

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-// MM-Menu Code
-	$('#menu').mmenu();
-	
-// http://baijs.com/tinycarousel/	
+    // MM-Menu Code
+    $('#menu').mmenu();
 
-$("#slider7").tinycarousel({ interval : true});
+    // http://baijs.com/tinycarousel/	
+
+    $("#slider7").tinycarousel({
+        interval: true
+    });
 
 
-// Built with	
-	console.log('This site was created using sass, susy, compass, breakpoints, bower, grunt, jquery, mmenu and npm:: by Thomas Ongeri');
-	
+    // Built with	
+    console.log('This site was created using sass, susy, compass, breakpoints, bower, grunt, jquery, mmenu and npm:: by Thomas Ongeri');
+
 });
